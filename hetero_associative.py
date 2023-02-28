Fix of weight calculation, which now is also relative to the mean.

hetero_associative.py
@@ -202,7 +202,7 @@ class HeteroAssociativeMemory:
         recognized = np.count_nonzero(
             r_io[:,:, :self.m, :self.q] == 0) <= self._xi
         weight = self._weight(vector_a, vector_b)
-        recognized = recognized and (self.mean*self._kappa <= weight)
+        recognized = recognized and (self._kappa <= weight)
         return recognized, weight
 
     def recall_from_left(self, vector):
@@ -257,14 +257,14 @@ class HeteroAssociativeMemory:
         return self.rows(dim) - 1
 
     def _weight(self, vector_a, vector_b):
-        return np.mean(self._weights(vector_a, vector_b))
+        return np.mean(self._weights(vector_a, vector_b))/self.mean
 
     def _weights(self, vector_a, vector_b):
         weights = []
         for i in range(self.n):
             for j in range(self.p):
                 w = self._relation[i,j,vector_a[i], vector_b[j]]
-            weights.append(w)
+                weights.append(w)
         return np.array(weights)
 
     def update(self):
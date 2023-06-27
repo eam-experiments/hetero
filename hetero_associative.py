Fix in plotting of confusion matrix for remembering, resizing of memory, min_max from standard deviation and cosmetics.

hetero_associative.py
@@ -243,7 +243,7 @@ class HeteroAssociativeMemory:
     def project(self, vector, weights, dim):
         integration = np.zeros((self.cols_alt(dim), self.rows_alt(dim)+1), dtype=int)
         columns = int(self.cols(dim)/10)
-        columns = (columns == 0) ? 1 : columns
+        columns = 1 if columns == 0 else columns
         used = []
         n = 0
         while n < columns:
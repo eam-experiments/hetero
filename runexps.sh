11bea51c367c11cad2d3a6a828d727b0d0604348
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2023-08-31 14:39:37 -0600

    runexps.sh complete (so far).

--- runexps.sh
+++ runexps.sh
@@ -5,6 +5,7 @@ ok=1
 echo "EAM Hetero experiments."
 echo "Storing results in $runpath"
 echo "=================== Starting..."
+date
 python eam.py -n mnist --runpath=$runpath && \
 python eam.py -n fashion --runpath=$runpath && \
 python eam.py -f mnist --runpath=$runpath && \
@@ -12,6 +13,9 @@ python eam.py -f fashion --runpath=$runpath && \
 python eam.py -s mnist --runpath=$runpath && \
 python eam.py -s fashion --runpath=$runpath && \
 python eam.py -e --runpath=$runpath && \
+python eam.py -v --runpath=$runpath && \
+python eam.py -w --runpath=$runpath && \
 python eam.py -r --runpath=$runpath
+date
 echo "=================== Done."
 

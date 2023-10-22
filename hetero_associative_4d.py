Fixes in recall recognition and distance calculation, updated _absolute_max.

hetero_associative_4d.py
@@ -42,7 +42,7 @@ class HeteroAssociativeMemory4D:
         self._p = p
         self._q = q+1 # +1 to handle partial functions.
         self._xi = es.xi
-        self._absolute_max = 2**32 - 1
+        self._absolute_max = 2**16 - 1
         self._sigma = es.sigma
         self._iota = es.iota
         self._kappa = es.kappa
@@ -58,7 +58,6 @@ class HeteroAssociativeMemory4D:
             f'm: {self.m}, q: {self.q}, ' +
             f'xi: {self.xi}, iota: {self.iota}, ' +
             f'kappa: {self.kappa}, sigma: {self.sigma}}}, has been created')
-
 
     def __str__(self):
         return f'{{n: {self.n}, p: {self.p}, m: {self.m}, q: {self.q},\n{self.rel_string}}}'
@@ -240,7 +239,7 @@ class HeteroAssociativeMemory4D:
         cue = self.validate(cue, dim)
         projection = self.project(cue, weights, dim)
         projection = self.transform(projection)
-        recognized = (np.count_nonzero(np.sum(projection, axis=1) == 0) <= self._xi)
+        recognized = (np.count_nonzero(np.sum(projection, axis=1) == 0) == 0)
         if not recognized:
             r_io = self.undefined_function(self.alt(dim))
             weight = 0.0
@@ -249,7 +248,6 @@ class HeteroAssociativeMemory4D:
         else:
             r_io, weights, iterations, dist_iters_mean = self.optimal_recall(cue, weights, projection, dim)
             weight = np.mean(weights)
-            recognized = (self._kappa*self.mean <= weight)
             r_io = self.revalidate(r_io, self.alt(dim))
         return r_io, recognized, weight, projection, iterations, dist_iters_mean
 
@@ -284,15 +282,14 @@ class HeteroAssociativeMemory4D:
             sum += self.calculate_distance(cue, cue_weights, p_io, dim)
             iterations += 1
             d = sum/iterations
-            if abs(d-distance) > 0.05*distance:
-                distance = d
-                n = 0
-            else:
-                n += 1
+            n = 0 if abs(d-distance) > 0.01*distance else n + 1
+            distance = d
         return d, iterations
 
     def calculate_distance(self, cue, cue_weights, p_io, dim):
         candidate, weights = self.reduce(p_io, dim)
+        candidate = np.array([t[0] if self.is_undefined(t[1], dim) else t[1]
+                              for t in zip(cue, candidate)])
         p = np.dot(cue_weights, weights)
         w = cue_weights*weights/p
         d = (cue-candidate)*w
@@ -348,7 +345,7 @@ class HeteroAssociativeMemory4D:
         dist = column
         s = dist.sum()
         if s == 0:
-            return random.randrange(dist.size)
+            return self.undefined(dim)
         r = s*random.random()
         for j in range(dist.size):
             if r <= dist[j]:
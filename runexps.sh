2f2de218b89e753c2be3851bb12232f78d0c15bc
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2024-01-11 22:03:50 -0600

    Parameterized runexps.sh

--- runexps.sh
+++ runexps.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-runpath="runs"
-ok=1
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 (3 | 4)"
+    exit 1
+fi
+
+dims=$1
+re='^[34]$'
+if ! [[ $dims =~ $re ]] ; then
+    echo "Usage: $0 (3 | 4)"
+    exit 2
+fi
+
+if [ $dims -eq 3 ]; then
+    runpath="runs_3d"
+else
+    runpath="runs_4d"
+fi
 echo "EAM Hetero experiments."
 echo "Storing results in $runpath"
 echo "=================== Starting..."
 date
-python eam.py -n mnist --runpath=$runpath && \
-python eam.py -n fashion --runpath=$runpath && \
-python eam.py -f mnist --runpath=$runpath && \
-python eam.py -f fashion --runpath=$runpath && \
-python eam.py -s mnist --runpath=$runpath && \
-python eam.py -s fashion --runpath=$runpath && \
-python eam.py -e --runpath=$runpath && \
-python eam.py -v --runpath=$runpath && \
-python eam.py -w --runpath=$runpath && \
-python eam.py -r --runpath=$runpath
+python eam.py -n mnist --dims=$dims --runpath=$runpath && \
+python eam.py -n fashion --dims=$dims --runpath=$runpath && \
+python eam.py -f mnist --dims=$dims --runpath=$runpath && \
+python eam.py -f fashion --dims=$dims --runpath=$runpath && \
+python eam.py -s mnist --dims=$dims --runpath=$runpath && \
+python eam.py -s fashion --dims=$dims --runpath=$runpath && \
+python eam.py -e --dims=$dims --runpath=$runpath && \
+python eam.py -v --dims=$dims --runpath=$runpath && \
+python eam.py -w --dims=$dims --runpath=$runpath && \
+python eam.py -r --dims=$dims --runpath=$runpath
+echo -n "=================== "
+ok=$?
 date
-echo "=================== Done."
+if [ $ok -eq 0 ]; then
+    echo "Done."
+else
+    echo "Sorry, something went wrong."
+fi
 

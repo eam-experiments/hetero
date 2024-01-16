1a0fd1259748c18aff9a59cfa7a7a88df41d41b3
Rafael Morales Gamboa <rmorales@suv.udg.mx>
2024-01-15 22:48:24 -0600

    Remembering states visited in search for best memory.

--- hetero_associative_4d.py
+++ hetero_associative_4d.py
@@ -261,11 +261,16 @@ class HeteroAssociativeMemory4D:
         r_io, weights = self.reduce(projection, self.alt(dim))
         distance, _ = self.distance_recall(cue, cue_weights, r_io, weights, dim)
         last_update = 0
-        for i, beta in zip(range(constants.n_sims), np.linspace(1.0, self.sigma, constants.n_sims)):
+        i = 0
+        visited = [r_io]
+        while i < constants.n_sims:
+            beta = 1.0 - (1.0 - self.sigma)*i/constants.n_sims
             s = self.rows(self.alt(dim)) * beta
             excluded = self.random_exclusion(r_io, p)
             s_projection = self.adjust(projection, r_io, s)
             q_io, q_ws = self.reduce(s_projection, self.alt(dim), excluded)
+            if np.any(np.all(q_io == visited, axis=1)):
+                continue
             d, _ = self.distance_recall(cue, cue_weights, q_io, q_ws, dim)
             if d < distance:
                 r_io = q_io
@@ -274,6 +279,7 @@ class HeteroAssociativeMemory4D:
                 iterations += 1
                 last_update = i
             p -= step
+            i += 1
         return r_io, weights, iterations, last_update
 
     def distance_recall(self, cue, cue_weights, q_io, q_ws, dim):

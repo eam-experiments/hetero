8014379d58b6d4038d1ef30a44547a982be9bc23
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2024-05-25 19:39:46 -0600

    Updates list of experiments

--- runexps.sh
+++ runexps.sh
@@ -25,19 +25,27 @@ python eam.py -s $right_ds --runpath=$runpath && \
 echo "------------------- `date`" && \
 python eam.py -e --runpath=$runpath && \
 echo "------------------- `date`" && \
-# python eam.py -v --runpath=$runpath && \
-# echo "------------------- `date`" && \
-# python eam.py -w --runpath=$runpath && \
-# echo "------------------- `date`" && \
+python eam.py -v --runpath=$runpath && \
+echo "------------------- `date`" && \
+python eam.py -w --runpath=$runpath && \
+echo "------------------- `date`" && \
 python eam.py -q --runpath=$runpath && \
-echo "------------------- `date`" && \
-python eam.py -P --runpath=$runpath && \
-echo "------------------- `date`" && \
-python eam.py -p --runpath=$runpath && \
 echo "------------------- `date`" && \
 python eam.py -r --runpath=$runpath && \
 echo "------------------- `date`" && \
-python eam.py -u --runpath=$runpath && \
+python eam.py -P constructed --runpath=$runpath && \
+echo "------------------- `date`" && \
+python eam.py -P recalled --runpath=$runpath && \
+echo "------------------- `date`" && \
+python eam.py -P extracted --runpath=$runpath && \
+echo "------------------- `date`" && \
+python eam.py -p constructed --runpath=$runpath && \
+echo "------------------- `date`" && \
+python eam.py -p recalled --runpath=$runpath && \
+echo "------------------- `date`" && \
+python eam.py -p extracted --runpath=$runpath && \
+# echo "------------------- `date`" && \
+# python eam.py -u --runpath=$runpath && \
 echo "=================== Ending at `date`"
 ok=$?
 if [ $ok -eq 0 ]; then

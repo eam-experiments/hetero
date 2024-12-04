88dd7fe3783b0282c069a048863c24d24437a674
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2024-12-04 14:02:38 -0600

    Fixes code

--- hetero_associative_4d.py
+++ hetero_associative_4d.py
@@ -413,7 +413,7 @@ class HeteroAssociativeMemory4D:
             print(f'Label: {label}, Chosen: None')
             return None, None, [0, 0, np.nan]
         am = AssociativeMemory.from_relation(projection, self.exp_settings_2d)
-        proto_label, proto_projection = self.choose_from_selection(
+        proto_label, proto_projection, distances = self.choose_from_selection(
             selection, projection, self.alt(dim)
         )
         recognized = False
@@ -424,7 +424,7 @@ class HeteroAssociativeMemory4D:
                 r_io = r_io.astype(int)
                 break
         print(
-            f'Label: {label}, Contained: {label in selection},  Chosen: {proto_label}, Tries: {n if recognized else 100}'
+            f'Label: {label}, Contained: {label in selection},  Chosen: {proto_label}, Distances: {distances}'
         )
         return (
             self.sample_n_search_recall(
@@ -574,14 +574,16 @@ class HeteroAssociativeMemory4D:
         chosen = None
         chosen_label = None
         distance = float('inf')
+        ds = {}
         for label in selection:
             proto_projection = selection[label]
             d = self.distance_projections(projection, proto_projection)
+            ds[label] = d
             if d < distance:
                 chosen = proto_projection
                 chosen_label = label
                 distance = d
-        return chosen_label, chosen
+        return chosen_label, chosen, ds
 
     def distance_projections(self, p, q):
         p_totals = np.sum(p, axis=1)
@@ -593,7 +595,7 @@ class HeteroAssociativeMemory4D:
     def probs_distances(self, p_probs, q_probs):
         distances = []
         for p, q in zip(p_probs, q_probs):
-            d = self.wessserstein_distance(p, q)
+            d = self.wasserstein_distance(p, q)
             distances.append(d)
         return distances
 
